66db89838f4961b947df4b9c3fced2f2daf14525
Nowfel <53569184+Nowfel@users.noreply.github.com>
2020-11-03 18:54:24 +0600

    navBar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ReactGa from "react-ga";
+import Home from "./components/Home/Home";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
+  useEffect(() => {
+    ReactGa.initialize("UA-168457671-1");
+
+    ReactGa.pageview(window.location.pathname);
+  }, []);
+
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/TopBanner/TopBanner.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import ReactGa from "react-ga";
+
+const TopBanner = () => {
+  const handleGA = () => {
+    ReactGa.event({
+      category: "Button",
+      action: "Join us clicked from home page",
+    });
+  };
+
+  return (
+    <div className="top-banner">
+      <div className="row">
+        <div className="col-md-6 p-5">
+          <h1 className="text-light">THE BEST FITNESS STUDIO IN TOWN</h1>
+          <p className="text-light">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </p>
+          <Link to="/">
+            <button className="btn btn-warning" onClick={handleGA}>
+              JOIN US
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopBanner;

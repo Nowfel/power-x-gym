b338d9f18a0f9145fbdea942d2154e398ce5d6ed
Nowfel <53569184+Nowfel@users.noreply.github.com>
2020-11-03 19:46:03 +0600

    ScheduleCard added

--- src/components/ClassDetails/ClassDetails.js
+++ src/components/ClassDetails/ClassDetails.js
@@ -1,22 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import ScheduleCard from "../ScheduleCard/ScheduleCard";
 
-const ClassDetails = ({details, hideDetails}) => {
-    return (
-        <div className='row pr-5 pl-5'>
-            <div className='col-md-6'>
-                <img src={details.bgImg} alt='...' style={{'height':'30vh'}}/>
-                <p>{details.summary}</p>
-                <ul>
-                    {details.features.map((feature,id) => <li key={id}>{feature}</li>)}
-                </ul>
-            </div>
-            
-            <div className='col-md-6'>
-                <h1>schedule coming</h1>
-            </div>
+const ClassDetails = ({ details, hideDetails }) => {
+  return (
+    <div className="row pr-5 pl-5">
+      <div className="col-md-6">
+        <img src={details.bgImg} alt="..." style={{ height: "30vh" }} />
+        <p>{details.summary}</p>
+        <ul>
+          {details.features.map((feature, id) => (
+            <li key={id}>{feature}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="col-md-6">
+        <h1>
+          <span className="text-warning">CLASS</span> SCHEDULE
+        </h1>
+        <div className="d-flex justify-content-between flex-wrap">
+          {details.schedule.map((s, id) => (
+            <ScheduleCard key={id} day={s.day} time={s.time} />
+          ))}
         </div>
-    );
+
+        <button className="btn btn-warning m-2" onClick={hideDetails}>
+          Classes
+        </button>
+        <Link to="/">
+          <button className="btn btn-warning m-2">Join Us</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default ClassDetails;
